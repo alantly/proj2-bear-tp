cf60e541fd27709d3ba9c04e2a69eb1a95831a99
Thanabordi Jianrungsang <sunsk128@gmail.com>
2014-10-30 01:06:12 -0700

    first working sliding window, but gotta write tests to test if it's actually working

--- Sender.py
+++ Sender.py
@@ -10,6 +10,7 @@ This is a skeleton sender class. Create a fantastic transport protocol here.
 class Sender(BasicSender.BasicSender):
 
     MAX_DATA_SIZE = 1472
+    WINDOW_SIZE = 5
 
     def __init__(self, dest, port, filename, debug=False, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
@@ -17,35 +18,43 @@ class Sender(BasicSender.BasicSender):
         if sackMode:
             raise NotImplementedError #remove this line when you implement SACK
 
+    def get_message_type(self, seqno, next_msg):
+        msg_type = 'data'
+        if seqno == 0:
+            msg_type = 'start'
+        elif next_msg == "":
+            msg_type = 'end'
+        return msg_type
+
     # Main sending loop.
     def start(self):
-        seqno = 0
+        seqno = 0    
         msg = self.infile.read(Sender.MAX_DATA_SIZE)
-
         msg_type = None
 
-        while not msg_type == 'end':
+        while seqno < self.WINDOW_SIZE and msg_type != 'end':
             next_msg = self.infile.read(Sender.MAX_DATA_SIZE)
-            msg_type = 'data'
-
-            if seqno == 0:
-                msg_type = 'start'
-            elif next_msg == "":
-                msg_type = 'end'
-
-            packet = self.make_packet(msg_type,seqno,msg)
+            msg_type = self.get_message_type(seqno, next_msg)
+            packet = self.make_packet(msg_type, seqno, msg)
             self.send(packet)
             self.window.append(packet)
             print "sent: %s" % packet
-
-            result = False
-            while not result:
-                response = self.receive(0.5)
-                result = self.handle_response(response)
-            self.window.pop(0)
             msg = next_msg
             seqno += 1
 
+        while len(self.window) > 0:
+            response = self.receive(0.5)
+            self.handle_response(response)
+            
+            if msg_type != 'end':
+                while msg_type != end and len(self.window) < self.WINDOW_SIZE:
+                    next_msg = self.infile.read(Sender.MAX_DATA_SIZE)
+                    msg_type = self.get_message_type(seqno, next_msg)
+                    packet = self.make_packet(msg_type, seqno, next_msg)
+                    self.send(packet)
+                    self.window.append(packet)
+                    print "sent: %s" % packet
+                    seqno += 1
         self.infile.close()
 
 
@@ -65,7 +74,11 @@ class Sender(BasicSender.BasicSender):
     def handle_new_ack(self, ack):
         if not Checksum.validate_checksum(ack):
             return False
-        return True
+        else:
+            ack_seq = str(int(self.split_packet(ack)[1]) - 1)
+            if ack_seq in [self.split_packet(x)[1] for x in self.window]:
+                while len(self.window) > 0 and int(self.split_packet(self.window[0])[1]) <= int(ack_seq):
+                    self.window.pop(0)
 
     def handle_dup_ack(self, ack):
         pass
